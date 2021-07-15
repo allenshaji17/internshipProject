7b191335625134b7e4183236091614834025ef6a
allenshaji17 <52339640+allenshaji17@users.noreply.github.com>
2021-07-15 16:42:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set('views',path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 //routes
-app.use('files/api',require('./routes/files'));
+app.use('/files/api',require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
 
