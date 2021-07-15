7be8ff0fc3c87d7b29f530cca839c3428b1c79c3
allenshaji17 <allenshaji.c@protonmail.com>
2021-07-16 00:07:25 +0530

    Final stages

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const connectDB = require('./config/db.js');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -10,12 +11,21 @@ app.use(express.static('public'));
 require('./config/db.js');
 connectDB();
 
+//cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+  }
+  app.use(cors(corsOptions));
+
+
+
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
 //routes
-app.use('/file/api',require('./routes/files'));
-app.use('/Files', require('./routes/show'));
+app.use('/files/api',require('./routes/files'));
+app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
 
 
