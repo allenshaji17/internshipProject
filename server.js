86ba242a439e074ad8b84925da50e693da213c4f
allenshaji17 <52339640+allenshaji17@users.noreply.github.com>
2021-07-15 18:39:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,12 +3,21 @@ const express = require('express');
 const connectDB = require('./config/db.js');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 
 require('./config/db.js');
 connectDB();
+
+//cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+    // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+  }
+  app.use(cors(corsOptions));
+
 
 app.set('views',path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
