7725583e0c4980b1e32c8d1705de60ecc3052a94
allenshaji17 <allenshaji.c@protonmail.com>
2021-07-15 06:47:09 +0530

    download page complete

--- server.js
+++ server.js
@@ -2,18 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db.js');
 const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
 require('./config/db.js');
 connectDB();
 
+app.set('views',path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
 
 //routes
 app.use('/file/api',require('./routes/files'));
+app.use('/Files', require('./routes/show'));
+
 
 
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+    console.log(`Listening on port ${PORT}.`);
+
 });
