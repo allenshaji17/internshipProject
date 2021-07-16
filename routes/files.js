ffc6e5e76284aa7947c8623579ac236414298233
allenshaji17 <52339640+allenshaji17@users.noreply.github.com>
2021-07-16 20:59:38 +0530

    Update files.js

--- routes/files.js
+++ routes/files.js
@@ -42,7 +42,7 @@ router.post('/',(req, res) => {
             });
 
             const response = await file.save();
-            return res.json({ file: `${process.env.APP_BASE_URL}files/${response.uuid}`});
+            res.json({ file: `${process.env.APP_BASE_URL}files/${response.uuid}`});
 
 
 
